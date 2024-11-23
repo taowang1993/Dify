8a6f2d5d9cbbac36bcee76b586307e60cec52c2e
Tao Wang <twang293@gmail.com>
2024-11-23 09:46:54 -0800

    Align Conversation Openers

--- web/app/components/base/chat/chat/answer/suggested-questions.tsx
+++ web/app/components/base/chat/chat/answer/suggested-questions.tsx
@@ -19,18 +19,20 @@ const SuggestedQuestions: FC<SuggestedQuestionsProps> = ({
     return null
 
   return (
-    <div className='flex flex-wrap'>
-      {suggestedQuestions.filter(q => !!q && q.trim()).map((question, index) => (
-        <div
-          key={index}
-          className='mt-1 mr-1 max-w-full last:mr-0 shrink-0 py-[5px] leading-[18px] items-center px-4 rounded-lg border border-gray-200 shadow-xs bg-white text-xs font-medium text-primary-600 cursor-pointer'
-          onClick={() => onSend?.(question)}
-        >
-          {question}
-        </div>),
-      )}
+    <div className="flex flex-wrap">
+      {suggestedQuestions
+        .filter((q) => !!q && q.trim())
+        .map((question, index) => (
+          <div
+            key={index}
+            className="mt-1 mr-1 min-w-[50%] max-w-full last:mr-0 shrink-0 py-[5px] leading-[18px] items-center px-4 rounded-lg border border-gray-200 shadow-xs bg-white text-xs font-medium text-primary-600 cursor-pointer"
+            onClick={() => onSend?.(question)}
+          >
+            {question}
+          </div>
+        ))}
     </div>
-  )
+  );
 }
 
 export default memo(SuggestedQuestions)
